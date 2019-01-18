766c469df6d9f5145790e1c2405b20d7f981f7be
rakesh.patel <rpatel@arke.com>
2019-01-18 16:51:42 -0500

    qcard added + data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import QuestionCard from './QuestionCard'
 
 class Dashboard extends Component {
 	render() {
+		const { userAnswers } = this.props 
+		
 		return (
 			<div>
 				<h3>Your Timeline</h3>
-				
+				<ul>
+				{ Object.keys(userAnswers).map((keyname) => (
+					<li>
+						<QuestionCard question={keyname} answer={userAnswers[keyname]}/>
+					</li>
+				))}
+				</ul>
+
 			</div>
 		)
 	}
 }
 
-function mapStateToProps({authedUser, users}) {
+function mapStateToProps({authedUser, users, questions}) {
 	//return object of user	
+	const userData = users[authedUser]
+
+	//const answeredQuestions = Object.keys(userData.answers)
 	return {
 		authedUser,
-		user: users[authedUser]
+		user: userData,
+		userAnswers: userData.answers 
 	}
 }
 
---
+++ src/components/QuestionCard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class QuestionCard extends Component {
+	render() {
+		return (
+			<p>Hey Rakesh</p>
+		)
+	}
+}
+
+export default QuestionCard
