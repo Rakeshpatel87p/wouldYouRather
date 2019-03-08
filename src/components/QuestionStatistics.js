b437d1f8ad5cbca315d70867583011036c16c314
rakesh.patel <rakeshpatel87p@gmail.com>
2019-03-07 20:29:04 -0500

    err being thrown - help pls

--- src/components/QuestionStatistics.js
+++ src/components/QuestionStatistics.js
@@ -31,13 +31,14 @@ class QuestionStatistics extends Component {
 
 function mapStateToProps({questions, users, authedUser}, props) {
 	//const qid = props.match.params - not working, unable iterate over obj
-	const qid ='vthrdm985a262al8qx3do'
+	//const qid ='am8ehyc8byjqgar0jgpub9'
+	const { qid } = props.match.param
 	console.log(qid)
 	const authToQuery = questions[qid].author
 
 	return {
 		questionData: questions[qid],
-		userData: users[authToQuery],
+		userData: users[questions[qid].author],
 		authedUser
 	}
 }
