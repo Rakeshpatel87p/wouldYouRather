3a81546d71387a5c5888b490e20c40d0ae22cf7c
rakesh.patel <rakeshpatel87p@gmail.com>
2019-03-07 19:40:50 -0500

    questionStat card created

--- src/components/QuestionStatistics.js
+++ src/components/QuestionStatistics.js
@@ -3,24 +3,32 @@ import { connect } from 'react-redux';
 
 class QuestionStatistics extends Component {
 	render() {
-		//const { name, avatarUrl } = this.props.userData
+
+		const { name, avatarUrl } = this.props.userData
+		const { optionOne, optionTwo } = this.props.questionData
+		const totalVotes = optionOne.votes.length + optionTwo.votes.length
 
 		return (
 			<div>
-				<h1>Results:</h1>
-
+				<p>Asked by {name}</p>
+				<img src={avatarUrl} />
+				<p>Would you rather {optionOne.text}?</p>
+				<p>{optionOne.votes.length} out of {totalVotes}</p>
+				<p>Would you rather {optionTwo.text}?</p>
+				<p>{optionTwo.votes.length} out of {totalVotes}</p>
 			</div>
 		)
 	}
 }
 
-function mapStateToProps({questions, users}, props) {
-	const [qid] = '8xf0y6ziyjabvozdd253nd'
+function mapStateToProps({questions, users, authedUser}, props) {
+	const qid = '8xf0y6ziyjabvozdd253nd'
+	const authToQuery = questions[qid].author
 	//const userToQuery = questions[qid].author
-	console.log(questions[qid])
+
 	return {
 		questionData: questions[qid],
-		//userData: users[questions[qid].author]
+		userData: users[authToQuery]
 	}
 }
 
